70815681826b34fd96d24040f4d396d98432316d
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-10-06 18:20:12 +0900

    php.iniに書いて表示させる方法

--- chapter3/chapter3-2/date.php
+++ chapter3/chapter3-2/date.php
@@ -5,6 +5,9 @@
   date_default_timezone_set( "Asia/Tokyo" );
   echo "現在は" . date( "G時 i分 s秒" ) . "です";
 
+  // php.iniを書き換える方法もある
+  // data.timezone = "Asia/Tokyo"
+
 // .を使うと文字の連結ができる
 // Rubyの + と一緒
 echo "<br>";
