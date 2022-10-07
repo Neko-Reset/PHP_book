fdc788094bce0b59e7989268f2383539adad183d
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-10-07 23:17:05 +0900

    formの値を受け取ろう

---
+++ chapter3/chapter3-17/form.php
@@ -0,0 +1,9 @@
+<!-- フォームに入力した内容を取得する -->
+<!-- 練習問題 -->
+<form action = "form.php" method = "GET" >
+  <label for = "your_name">お名前</label>
+  <input type = "text" id = "your_name" name = "your_name" maxlength = "6" value = "">
+  <label for = "memo">メモ</label>
+  <input type = "text" id = "memo" name = "memo" maxlength = "10" value = "">
+  <input type = "submit" value = "送信する">
+</form>
---
+++ chapter3/chapter3-17/index.php
@@ -0,0 +1,14 @@
+お名前:<?php echo htmlspecialchars( $_GET[ "your_name" ] , ENT_QUOTES ); ?>
+メモ:<?php echo htmlspecialchars( $_GET[ "memo" ] , ENT_QUOTES ); ?>
+
+<?php
+// $_REQUEST = グローバル変数
+// $_REQUESTは$_GET $_POSTに関わらず利用できる
+// 危険性が増すから基本的に$_GET $_POSTで分ける
+
+// htmlspecialcharsでjs対策
+// ENT_QUOTESはシングルクオーテーションを安全に受け取る
+// 安全性が増すから基本つける
+
+// PHPの設定でmagic_quotes_gpcというのがあり\で""を無効化する能力を自動的に無害化している
+?>
